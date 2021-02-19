c986b5a28235f663437b48813e8a3ed2860a3a34
Аймкулова Ботагоз <botagoz.aimkulova@gmail.com>
2021-02-19 15:59:39 +0600

    Удален строгий режим из index.js

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -6,50 +6,50 @@ import Spinner from "../../components/UI/Spinner/Spinner";
 import withErrorHandler from "../../HOC/withErrorHandler";
 
 const Home = (props) => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
+	const [data, setData] = useState([]);
+	const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    setLoading(true);
-    let fetchData = async () => {
-      try {
-        let response = await axiosBlog.get(`.`);
-        response = Object.keys(response.data).map((id) => {
-          return { ...response.data[id], id };
-        });
-        setData(response);
-      } catch (e) {
-        console.error(e);
-      }
-    };
-    fetchData().finally(() => setLoading(false));
-  }, []);
+	useEffect(() => {
+		setLoading(true);
+		let fetchData = async () => {
+			try {
+				let response = await axiosBlog.get(`.`);
+				response = Object.keys(response.data).map((id) => {
+					return { ...response.data[id], id };
+				});
+				setData(response);
+			} catch (e) {
+				console.error(e);
+			}
+		};
+		fetchData().finally(() => setLoading(false));
+	}, []);
 
-  let jumpSinglePage = (id) => {
-    return props.history.push("/posts/" + id);
-  };
+	let jumpSinglePage = (id) => {
+		return props.history.push("/posts/" + id);
+	};
 
-  let renderBlogs = data.map((item) => {
-    return (
-      <Card key={item.id}>
-        <CardBody>
-          <CardSubtitle tag='h6' className='mb-2 text-muted'>
-            {item.date}
-          </CardSubtitle>
-          <CardTitle tag='h5'>{item.title}</CardTitle>
-          <Button color='success' onClick={() => jumpSinglePage(item.id)}>
-            Read more &gt;&gt;
-          </Button>
-        </CardBody>
-      </Card>
-    );
-  });
-  return (
-    <div className='blog_wrapper'>
-      {loading ? <Spinner /> : null}
-      {renderBlogs}
-    </div>
-  );
+	let renderBlogs = data.map((item) => {
+		return (
+			<Card key={item.id}>
+				<CardBody>
+					<CardSubtitle tag='h6' className='mb-2 text-muted'>
+						{item.date}
+					</CardSubtitle>
+					<CardTitle tag='h5'>{item.title}</CardTitle>
+					<Button color='success' onClick={() => jumpSinglePage(item.id)}>
+						Read more &gt;&gt;
+					</Button>
+				</CardBody>
+			</Card>
+		);
+	});
+	return (
+		<div className='blog_wrapper'>
+			{loading ? <Spinner /> : null}
+			{renderBlogs}
+		</div>
+	);
 };
 
 export default withErrorHandler(Home, axiosBlog);
--- src/index.js
+++ src/index.js
@@ -7,12 +7,10 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
-  document.getElementById("root")
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
